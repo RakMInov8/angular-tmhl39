f2174aade8e6d96588ed1d88e9c3ea9548b6d03c
RakMInov8 <rakesh.mehta@inov8.co.nz>
2019-10-09 15:31:06 +1300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, from} from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements onInit  {
   name = 'Angular';
+
+ngonInit(){
+  of(2,4,6,8).subscribe(console.log);
+
+from([20, 15, 10, 5]).subscribe(
+  item => console.log('resulting item ..${item}',
+  err => console.error('error occureed ${err}'),
+  () => console.log('Complete')
+  );
+
+
+
 }
+
+}
+
