f0cbed36aef61214d95b71a175a6ecbc9b38a4d1
RakMInov8 <rakesh.mehta@inov8.co.nz>
2019-10-10 14:09:11 +1300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {of, from} from 'rxjs';
-
+import {map,tap,take} from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -12,17 +12,22 @@ export class AppComponent implements OnInit  {
 ngOnInit(){
   of(2,4,6,8).subscribe(console.log);
 
-from([20, 15, 10, 5]).subscribe(
-  item => console.log(`resulting item ..${item}`),
-  err => console.error(`error occureed ${err}`),
-  () => console.log('Complete')
+from([20, 15, 10, 5])
+  .pipe(
+        tap(item => console.log(`emitted item ..${item}`)),
+        map(item => item*2),
+        map(item => item-10)
+  ).subscribe(
+    item => console.log(`resulting item ..${item}`),
+    err => console.error(`error occureed ${err}`),
+    () => console.log('Complete')
   );
 
-  of('Apple1', 'Apple2', 'Apple3')
+  of('Rakesh', 'Denis', 'Mike')
     .subscribe(
-      apple => console.log('Apple was emiited ${apple}'),
-      err => console.error('Error occured: ${err}'),
-      () => console.log('No more apples, go home')
+      emp => console.log(`Employees was emiited ${emp}`),
+      err => console.error(`Error occured: ${err}`),
+      () => console.log(`No more apples, go home`)
     );
   }
 }
